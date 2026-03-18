ab9dad0c7ff83f70fa610eb058cafd767f0a897f
paulinaroldan <141651058+paulinaroldan@users.noreply.github.com>
2026-03-18 18:49:49 -0300

    hice metodos en archivoBibliotecaReal
    hice escribirArchivo(), mostrarArchivo()

--- src/Modelo/ArchivoBibliotecaReal.java
+++ src/Modelo/ArchivoBibliotecaReal.java
@@ -1,4 +1,36 @@
 package Modelo;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class ArchivoBibliotecaReal {
+    private final String archivo = "armasForjadas";
+
+    //serializar
+    public void objetoAjson(){
+
+    }
+    public void escribirArchivo(String nombreArchivo) {
+        try {
+            BufferedWriter escribir = new BufferedWriter(new FileWriter(nombreArchivo + ".json"));
+            escribir.write(jsonArr.toString());
+            escribir.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String mostrarArchivo(String nombreArchivo) {
+        String rta = "";
+        File lectura = new File(nombreArchivo + ".json");
+        try {
+            rta+= Files.readString(lectura.toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return rta;
+    }
 }
